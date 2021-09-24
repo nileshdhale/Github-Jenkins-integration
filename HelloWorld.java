bb1182af08788e21c80f3975af1640fadf137ff1
nilesh.rkr@gmail.com <nilesh.rkr@gmail.com>
2021-09-24 08:29:21 -0400

    java commit

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	int n = 1;
+	while(n<10)
+	{
+		System.out.println("learn coding");
+		n++;
+	}
+	
+	
+	}
+		
+		
+
+	}
+
